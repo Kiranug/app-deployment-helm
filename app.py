dea4630afdd14d36a741f1c9ec1b8c44881764d2
kiranug <kiran.ug123@gmail.com>
2025-04-24 12:53:24 +0530

    vgh

--- app.py
+++ app.py
@@ -38,3 +38,6 @@ if __name__ == '__main__':
 
     # Note: Set debug=False in production
     app.run(debug=True)
+
+    if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
